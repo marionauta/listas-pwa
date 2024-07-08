a377ae8d347444f7e351a6648a28bdf1a8a897b8
Mario Nachbaur <marionauta@users.noreply.github.com>
2024-07-08 17:40:31 +0200

    feat: auto pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA as pwa } from "vite-plugin-pwa";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
   base: "/",
-  plugins: [wasm(), react()],
+  plugins: [
+    wasm(),
+    react(),
+    pwa({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,ts,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        id: "/",
+        name: "Lists",
+        start_url: "/",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "1024x1024",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: { enabled: true },
+    }),
+  ],
 });
